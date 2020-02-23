ced5d56ef741d4266c523c12bd365361ca36f8e2
ghilbut <ghilbut@gmail.com>
2020-02-23 16:56:59 +0900

    #1 Create Vue.js project (#6)
    * #1 Create Vue.js project
    
    * #1 Apply Vuetify.js with Material Icons
    
    * #1 Apply Login Form with Material Icons

---
+++ vue.js/src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <v-app>
+    <v-app-bar app color="primary" dark>
+      <div class="d-flex align-center">
+        <v-img
+          alt="Vuetify Logo"
+          class="shrink mr-2"
+          contain
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
+          transition="scale-transition"
+          width="40"
+        />
+
+        <v-img
+          alt="Vuetify Name"
+          class="shrink mt-1 hidden-sm-and-down"
+          contain
+          min-width="100"
+          src="https://cdn.vuetifyjs.com/images/logos/vuetify-name-dark.png"
+          width="100"
+        />
+      </div>
+
+      <v-spacer></v-spacer>
+
+      <v-btn text to="/">Home</v-btn> |
+      <v-btn text to="/about">About</v-btn>
+
+      <v-spacer></v-spacer>
+
+      <v-btn text to="/login">Login</v-btn>
+    </v-app-bar>
+
+    <v-content>
+      <router-view />
+    </v-content>
+  </v-app>
+</template>
+
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  name: "App",
+
+  data: () => ({
+    //
+  })
+});
+</script>
