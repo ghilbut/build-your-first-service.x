27c5158932ebbf624095b13f70c6cd21c0c06c76
ghilbut <ghilbut@gmail.com>
2020-02-26 18:20:01 +0900

    Apply custom user model for authentication

---
+++ django/apps/authex/__init__.py
@@ -0,0 +1,1 @@
+default_app_config = 'apps.authex.apps.AuthExConfig'
--- django/byfs/settings.py
+++ django/byfs/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'apps.authex',
 ]
 
 MIDDLEWARE = [
@@ -130,3 +131,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+
+AUTH_USER_MODEL = 'authex.User'
