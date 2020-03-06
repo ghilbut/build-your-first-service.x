41c356da4004aa6d844deaf3cde32dd005fa55d1
ghilbut <ghilbut@gmail.com>
2020-03-06 19:11:09 +0900

    final

--- django/byfs/settings/develop.py
+++ django/byfs/settings/develop.py
@@ -6,20 +6,14 @@ ALLOWED_HOSTS = [
 ]
 
 
-#DATABASES = {
-#    'default': {
-#        'ENGINE': 'django.db.backends.mysql',
-#        'NAME': os.environ['BYFS_DB_DEVELOP_NAME'],
-#        'HOST': os.environ['BYFS_DB_DEVELOP_HOST'],
-#        'PORT': os.environ['BYFS_DB_DEVELOP_PORT'],
-#        'USER': os.environ['BYFS_DB_DEVELOP_USER'],
-#        'PASSWORD': os.environ['BYFS_DB_DEVELOP_PASSWORD'],
-#        'CONN_MAX_AGE': 60,
-#    }
-#}
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': os.environ['BYFS_DB_DEVELOP_NAME'],
+        'HOST': os.environ['BYFS_DB_DEVELOP_HOST'],
+        'PORT': os.environ['BYFS_DB_DEVELOP_PORT'],
+        'USER': os.environ['BYFS_DB_DEVELOP_USER'],
+        'PASSWORD': os.environ['BYFS_DB_DEVELOP_PASSWORD'],
+        'CONN_MAX_AGE': 60,
     }
 }
